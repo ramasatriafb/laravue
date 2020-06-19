c675178386c47df78a3d73079cc86d64246ada64
Rama Firmansyah <ramasatriafb@gmail.com>
2020-06-19 09:24:50 +0700

    First Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <Hero/>
+    <WomanShayna/>
+    <InstagramSection/>
+    <PartnerSection/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import Hero from '@/components/Hero.vue'
+import WomanShayna from '@/components/WomanShayna.vue'
+import InstagramSection from '@/components/InstagramSection.vue'
+import PartnerSection from '@/components/PartnerSection.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    Hero,
+    WomanShayna,
+    InstagramSection,
+    PartnerSection,
+    Footer
+  }
+}
+</script>
